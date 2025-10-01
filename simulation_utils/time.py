abdc65eb98f399117db0339c8afefb1b9834e687
alx <agarkov.aleksei1@yandex.ru>
2025-10-02 00:13:41 +0300

    Refactor time utils

--- simulation_utils/time.py
+++ simulation_utils/time.py
@@ -3,6 +3,12 @@ import zoneinfo
 
 MSK = zoneinfo.ZoneInfo("Europe/Moscow")
 
+DATETIME_FORMAT = "%Y-%m-%d-%H%M%SZ"
 
-def msk_now_str():
-    return datetime.now(MSK).strftime("%Y-%m-%d-%H%M%SZ")
+
+def msk_now_str(format=DATETIME_FORMAT):
+    return datetime.now(MSK).strftime(format)
+
+
+def msk_now():
+    return datetime.now(MSK)
