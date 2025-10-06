034611f2de2b520ecad1b7a466595cff980c3fa6
alx <agarkov.aleksei1@yandex.ru>
2025-10-06 22:53:01 +0300

    Implement `convert_transitions_to_agent_states` converter

--- src/analysis/converters.py
+++ src/analysis/converters.py
@@ -1,6 +1,7 @@
 from copy import deepcopy
 from typing import List
 
+import numpy as np
 import pandas as pd
 
 
@@ -33,3 +34,43 @@ def convert_transitions_to_state_counts(transitions_df: pd.DataFrame,
     for column in result_df.columns:
         result_df[column] = result_df[column].astype(int)
     return result_df
+
+
+def cartesian_product(*arrays):
+    la = len(arrays)
+    dtype = np.result_type(*arrays)
+    arr = np.empty([len(a) for a in arrays] + [la], dtype=dtype)
+    for i, a in enumerate(np.ix_(*arrays)):
+        arr[..., i] = a
+    return arr.reshape(-1, la)
+
+
+def convert_transitions_to_agent_states(transitions_df: pd.DataFrame,
+                                        states: List[str],
+                                        initial_state: str,
+                                        n_agents: int,
+                                        sim_duration: int) -> pd.DataFrame:
+    states_numeric = {state: i for i, state in enumerate(states)}
+
+    time = pd.DataFrame(range(sim_duration), columns=['time'])
+
+    transitions_data = (
+        transitions_df[['time', 'agent', 'dest_state']]
+        .assign(dest_state=lambda _df: _df.dest_state.map(states_numeric))
+        .pivot_table(index='time', columns='agent', values='dest_state', aggfunc='last')
+        .reset_index()
+        .rename_axis(None, axis=1)
+        .merge(time, on='time', how='right')
+    )
+    agent_cols = range(n_agents)
+    transitions_data.loc[0, agent_cols] = transitions_data.loc[0, agent_cols].fillna(states_numeric[initial_state])
+    transitions_data = (
+        transitions_data
+        .ffill()
+        .melt(id_vars='time', var_name='agent', value_name='state')
+        .astype({'state': 'uint8', 'agent': 'uint64'})
+        .sort_values(by=['time', 'agent'])
+        .reset_index(drop=True)
+    )
+
+    return transitions_data
--- tests/analysis/test_converters.py
+++ tests/analysis/test_converters.py
@@ -2,28 +2,58 @@ from unittest import TestCase
 
 import pandas as pd
 
-from src.analysis.converters import convert_transitions_to_state_counts
+from src.analysis.converters import convert_transitions_to_state_counts, convert_transitions_to_agent_states
 
 transitions_data = [
-    {'time': 1, 'source_state': 'susceptible', 'dest_state': 'infected'},
-    {'time': 3, 'source_state': 'susceptible', 'dest_state': 'infected'},
-    {'time': 3, 'source_state': 'susceptible', 'dest_state': 'infected'},
-    {'time': 5, 'source_state': 'infected', 'dest_state': 'recovered'},
-    {'time': 6, 'source_state': 'infected', 'dest_state': 'recovered'},
-    {'time': 6, 'source_state': 'infected', 'dest_state': 'recovered'},
+    {'time': 0, 'agent': 0, 'source_state': 'susceptible', 'dest_state': 'infected'},
+    {'time': 2, 'agent': 1, 'source_state': 'susceptible', 'dest_state': 'infected'},
+    {'time': 3, 'agent': 2, 'source_state': 'susceptible', 'dest_state': 'infected'},
+    {'time': 5, 'agent': 0, 'source_state': 'infected', 'dest_state': 'recovered'},
+    {'time': 6, 'agent': 1, 'source_state': 'infected', 'dest_state': 'recovered'},
+    {'time': 6, 'agent': 2, 'source_state': 'infected', 'dest_state': 'recovered'},
 ]
 transitions_df = pd.DataFrame(transitions_data)
-test_data = [
-    {'time': 0, 'susceptible': 3, 'infected': 0, 'recovered': 0},
+
+state_counts_test_data = [
+    {'time': 0, 'susceptible': 2, 'infected': 1, 'recovered': 0},
     {'time': 1, 'susceptible': 2, 'infected': 1, 'recovered': 0},
-    {'time': 2, 'susceptible': 2, 'infected': 1, 'recovered': 0},
+    {'time': 2, 'susceptible': 1, 'infected': 2, 'recovered': 0},
     {'time': 3, 'susceptible': 0, 'infected': 3, 'recovered': 0},
     {'time': 4, 'susceptible': 0, 'infected': 3, 'recovered': 0},
     {'time': 5, 'susceptible': 0, 'infected': 2, 'recovered': 1},
     {'time': 6, 'susceptible': 0, 'infected': 0, 'recovered': 3},
     {'time': 7, 'susceptible': 0, 'infected': 0, 'recovered': 3}
 ]
-test_df = pd.DataFrame(test_data)
+state_counts_test_df = pd.DataFrame(state_counts_test_data)
+
+agent_states_test_data = [{'time': 0, 'agent': 0, 'state': 1},
+                          {'time': 0, 'agent': 1, 'state': 0},
+                          {'time': 0, 'agent': 2, 'state': 0},
+                          {'time': 1, 'agent': 0, 'state': 1},
+                          {'time': 1, 'agent': 1, 'state': 0},
+                          {'time': 1, 'agent': 2, 'state': 0},
+                          {'time': 2, 'agent': 0, 'state': 1},
+                          {'time': 2, 'agent': 1, 'state': 1},
+                          {'time': 2, 'agent': 2, 'state': 0},
+                          {'time': 3, 'agent': 0, 'state': 1},
+                          {'time': 3, 'agent': 1, 'state': 1},
+                          {'time': 3, 'agent': 2, 'state': 1},
+                          {'time': 4, 'agent': 0, 'state': 1},
+                          {'time': 4, 'agent': 1, 'state': 1},
+                          {'time': 4, 'agent': 2, 'state': 1},
+                          {'time': 5, 'agent': 0, 'state': 2},
+                          {'time': 5, 'agent': 1, 'state': 1},
+                          {'time': 5, 'agent': 2, 'state': 1},
+                          {'time': 6, 'agent': 0, 'state': 2},
+                          {'time': 6, 'agent': 1, 'state': 2},
+                          {'time': 6, 'agent': 2, 'state': 2},
+                          {'time': 7, 'agent': 0, 'state': 2},
+                          {'time': 7, 'agent': 1, 'state': 2},
+                          {'time': 7, 'agent': 2, 'state': 2}]
+
+agent_states_test_df = pd.DataFrame(agent_states_test_data)
+agent_states_test_df['state'] = agent_states_test_df['state'].astype('uint8')
+agent_states_test_df['agent'] = agent_states_test_df['agent'].astype('uint64')
 
 
 class Test(TestCase):
@@ -33,4 +63,14 @@ class Test(TestCase):
                                                         initial_state='susceptible',
                                                         n_agents=3,
                                                         sim_duration=8)
-        self.assertTrue(test_df.equals(result_df))
+        self.assertTrue(state_counts_test_df.equals(result_df))
+
+    def test_convert_transitions_to_agent_states(self):
+        result_df = convert_transitions_to_agent_states(transitions_df=transitions_df,
+                                                        states=['susceptible', 'infected', 'recovered'],
+                                                        initial_state='susceptible',
+                                                        n_agents=3,
+                                                        sim_duration=8)
+        print(result_df)
+        # print(agent_states_test_df.compare(result_df))
+        self.assertTrue(agent_states_test_df.equals(result_df))
