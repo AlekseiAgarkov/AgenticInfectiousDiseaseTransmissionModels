e26abe45425e58c2b4fbe201414cedb29dead9c3
alx <agarkov.aleksei1@yandex.ru>
2025-11-25 00:54:50 +0300

    Implement LinearPathogen

--- src/models/pathogen.py
+++ src/models/pathogen.py
@@ -1,5 +1,7 @@
 from abc import ABC, abstractmethod
+from typing import Any, Union
 
+import numpy as np
 import simpy
 
 
@@ -13,5 +15,25 @@ class Pathogen(ABC):
         return self._get_beta(sim_time=self.env.now)
 
     @abstractmethod
-    def _get_beta(self, sim_time: int) -> float:
+    def _get_beta(self, sim_time: int) -> Union[float, np.float64]:
         pass
+
+
+class LinearPathogen(Pathogen):
+    pathogen_values: np.ndarray[tuple[Any, ...], np.dtype[np.float64]]
+
+    def __init__(self,
+                 env: simpy.Environment,
+                 name: str,
+                 sim_duration: int,
+                 base_beta: float,
+                 max_beta: float):
+        super().__init__(env, name)
+        self.sim_duration = sim_duration
+        self.base_beta = base_beta
+        self.max_beta = max_beta
+
+        self.pathogen_values = np.linspace(start=base_beta, num=sim_duration + 1, stop=max_beta)
+
+    def _get_beta(self, sim_time: int) -> Union[float, np.float64]:
+        return self.pathogen_values[sim_time]
---
+++ tests/models/test_pathogen.py
@@ -0,0 +1,25 @@
+from unittest import TestCase
+from unittest.mock import MagicMock
+
+from models.pathogen import LinearPathogen
+
+
+class LinearPathogenTests(TestCase):
+
+    def test_linear_pathogen(self):
+        env = MagicMock()
+
+        base_beta = 0.0
+        increment = 0.1
+        sim_duration = 10
+        max_beta = sim_duration * increment
+
+        linear_pathogen = LinearPathogen(env=env,
+                                         name='linear_pathogen',
+                                         sim_duration=sim_duration,
+                                         base_beta=base_beta,
+                                         max_beta=max_beta)
+
+        for sim_step in range(sim_duration + 1):
+            env.now = sim_step
+            assert linear_pathogen() == (increment * sim_step + base_beta)
