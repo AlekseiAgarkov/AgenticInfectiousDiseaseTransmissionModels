40f354f87c28700c33740e6230f0b5be7a4cfdfc
alx <agarkov.aleksei1@yandex.ru>
2025-12-01 22:12:26 +0300

    Polygon is optional in point_in_polygon

--- src/models/polygons.py
+++ src/models/polygons.py
@@ -1,5 +1,10 @@
+from typing import Optional
+
 from matplotlib import path as mpl_path
 
 
-def point_in_polygon(x, y, polygon: mpl_path.Path):
+def point_in_polygon(x, y, polygon: Optional[mpl_path.Path]):
+    if not polygon:
+        return False
+
     return polygon.contains_point((x, y))
--- tests/models/test_polygons.py
+++ tests/models/test_polygons.py
@@ -7,6 +7,7 @@ from models.polygons import point_in_polygon
 
 
 class PolygonsTests(TestCase):
+    empty_polygon = None
     polygon = mpl_path.Path(np.array([[0, 0], [0, 10], [10, 10], [10, 0]], dtype=np.float32))
     inside_polygon = np.array([[1, 1], [5, 5]], dtype=np.float32)
     outside_polygon = np.array([[-1, -1], [-1, 11], [11, 11], [11, -1]], dtype=np.float32)
@@ -20,3 +21,8 @@ class PolygonsTests(TestCase):
         for point in self.outside_polygon:
             x, y = point[0], point[1]
             assert not point_in_polygon(x, y, self.polygon)
+
+    def test_always_false_for_empty_polygon(self):
+        for point in self.inside_polygon:
+            x, y = point[0], point[1]
+            assert not point_in_polygon(x, y, self.empty_polygon)
